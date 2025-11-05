b4a1136752e7fdba2a601118d96ba5f423ccafcf
Jinyeungseo <rkskek1101@gmail.com>
2025-11-05 11:32:35 +0900

    - aiTest에 import 추가 - QueryRequest.java 생성

---
+++ backend/src/main/java/com/project/eum/controller/QueryRequest.java
@@ -0,0 +1,17 @@
+package com.project.eum.controller;
+
+public class QueryRequest {
+
+    // QueryRequest 클래스도 필요
+
+        private String query;
+
+        public String getQuery() {
+            return query;
+        }
+
+        public void setQuery(String query) {
+            this.query = query;
+        }
+
+}
--- backend/src/main/java/com/project/eum/controller/aiTest.java
+++ backend/src/main/java/com/project/eum/controller/aiTest.java
@@ -1,5 +1,11 @@
 package com.project.eum.controller;
 
+// 이 부분이 없어서 에러! 추가 필요!
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
 @RestController
 @RequestMapping("/api/ai")
 public class aiTest {
