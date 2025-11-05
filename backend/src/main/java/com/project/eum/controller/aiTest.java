1099e0180fcf24f43b4893bd051e8c35fe60b82c
Jinyeungseo <rkskek1101@gmail.com>
2025-11-05 11:36:34 +0900

    - aiTest 코드 수정 - QueryRequest.java 삭제

--- backend/src/main/java/com/project/eum/controller/aiTest.java
+++ backend/src/main/java/com/project/eum/controller/aiTest.java
@@ -1,27 +1,15 @@
 package com.project.eum.controller;
 
-// 이 부분이 없어서 에러! 추가 필요!
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/ai")
 public class aiTest {
 
-    @Value("${ai.server.url}")
-    private String aiServerUrl;
-
-    @PostMapping("/query")
-    public ResponseEntity<?> queryAI(@RequestBody QueryRequest request) {
-        RestTemplate restTemplate = new RestTemplate();
-        String url = aiServerUrl + "/query";
-
-        ResponseEntity<String> response = restTemplate.postForEntity(
-                url, request, String.class
-        );
-
-        return ResponseEntity.ok(response.getBody());
+    @GetMapping("/test")
+    public String test() {
+        return "AI Server Connection Test";
     }
 }
