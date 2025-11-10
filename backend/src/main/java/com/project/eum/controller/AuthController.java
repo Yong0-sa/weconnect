99c9b2d91e1b8615cd60cda362b1551defad407d
Jinyeungseo <rkskek1101@gmail.com>
2025-11-10 15:56:43 +0900

    authcontroller 백엔드 요청 경로 수정

--- backend/src/main/java/com/project/eum/controller/AuthController.java
+++ backend/src/main/java/com/project/eum/controller/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping({"/api/auth", "/auth"})
 public class AuthController {
 
     private final String defaultEmail;
