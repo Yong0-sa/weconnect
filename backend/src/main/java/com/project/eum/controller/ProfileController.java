d90e02e0537583ded27ebbf1f57f278ec2ee06e0
Jinyeungseo <rkskek1101@gmail.com>
2025-11-11 11:07:20 +0900

    - api 주소 변경

--- backend/src/main/java/com/project/eum/controller/ProfileController.java
+++ backend/src/main/java/com/project/eum/controller/ProfileController.java
@@ -14,7 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/profile")
+@RequestMapping({"/api/profile", "/profile"})
 @RequiredArgsConstructor
 public class ProfileController {
 
