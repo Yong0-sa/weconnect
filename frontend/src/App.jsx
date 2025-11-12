4271419388c5539268f6bb1ccffaf2849f13a367
IM JINSEO <jsim2549@gmail.com>
2025-11-12 19:57:32 +0900

    필요없는 페이지 삭제(profilecomplete.jsx)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,6 @@ import TutorialPage from "./pages/TutorialPage";
 import LoginPage from "./pages/LoginPage";
 import ChatPage from "./pages/ChatPage";
 import OAuthCallbackPage from "./pages/OAuthCallbackPage";
-import ProfileCompletePage from "./pages/ProfileCompletePage";
 import "./App.css";
 
 function App() {
@@ -21,7 +20,6 @@ function App() {
         <Route path="/community" element={<div>커뮤니티 (준비중)</div>} />
         <Route path="/chat" element={<ChatPage />} />
         <Route path="/oauth/success" element={<OAuthCallbackPage />} />
-        <Route path="/profile/complete" element={<ProfileCompletePage />} />
       </Routes>
     </Router>
   );
