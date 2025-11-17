21a76817d936680b9471cea141295c6d6d883f25
IM JINSEO <jsim2549@gmail.com>
2025-11-17 14:58:16 +0900

    회원등록 기능 api 경로 수정

--- backend/src/main/java/com/project/eum/controller/FarmContractController.java
+++ backend/src/main/java/com/project/eum/controller/FarmContractController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/farm-contracts")
+@RequestMapping({"/api/farm-contracts", "/farm-contracts"})
 @RequiredArgsConstructor
 public class FarmContractController {
 
