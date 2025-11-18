6cded702c2dcb5a0805c827cb615fbf7dcf7deb3
Jinyeungseo <rkskek1101@gmail.com>
2025-11-18 19:08:17 +0900

    - 댓글 api 경로 수정

--- backend/src/main/java/com/project/eum/controller/CommentsController.java
+++ backend/src/main/java/com/project/eum/controller/CommentsController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/comments")
+@RequestMapping({"/api/comments", "/comments"})
 @RequiredArgsConstructor
 public class CommentsController {
 
