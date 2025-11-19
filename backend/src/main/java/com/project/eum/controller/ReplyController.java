85eb88cd191b036469ee7bbb903e17e8d17f7337
Jinyeungseo <rkskek1101@gmail.com>
2025-11-19 14:36:00 +0900

    - 매핑 경로 수정

--- backend/src/main/java/com/project/eum/controller/ReplyController.java
+++ backend/src/main/java/com/project/eum/controller/ReplyController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/replies")
+@RequestMapping({"/api/replies","/replies"})
 @RequiredArgsConstructor
 public class ReplyController {
 
